a0553be2317f253ac61ee650eff536bbd655a093
tonyson97 <notsimple99@gmail.com>
2021-11-03 00:22:35 +0700

    deploy to github pages

---
+++ build/precache-manifest.ac043f80c468b516bc3942a1a36c4c41.js
@@ -0,0 +1,334 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4e5f4010d64e64fec84c702bf92565",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "fb062a4890741a8bb108",
+    "url": "/ecommerce/static/css/9.dde4a899.chunk.css"
+  },
+  {
+    "revision": "b47e5b8519c1b9510524",
+    "url": "/ecommerce/static/css/main.d71730a9.chunk.css"
+  },
+  {
+    "revision": "dcbf8f08d896a1417d93",
+    "url": "/ecommerce/static/js/0.d50fb0cd.chunk.js"
+  },
+  {
+    "revision": "7a836d1f1b22471a666f",
+    "url": "/ecommerce/static/js/1.66b74f6f.chunk.js"
+  },
+  {
+    "revision": "38147c77a6714407bf27",
+    "url": "/ecommerce/static/js/10.8af5ecb2.chunk.js"
+  },
+  {
+    "revision": "200dc2942220cdd041eb",
+    "url": "/ecommerce/static/js/11.64ea4db4.chunk.js"
+  },
+  {
+    "revision": "64141c39ff46a45b326c",
+    "url": "/ecommerce/static/js/12.1c6e83aa.chunk.js"
+  },
+  {
+    "revision": "101d0e43b3bfec1d0580",
+    "url": "/ecommerce/static/js/13.c2baf1e7.chunk.js"
+  },
+  {
+    "revision": "6bdb37caf9a239c7ce03",
+    "url": "/ecommerce/static/js/14.52f7aa45.chunk.js"
+  },
+  {
+    "revision": "87e49ada3ce0016d8eb9",
+    "url": "/ecommerce/static/js/15.b68ead24.chunk.js"
+  },
+  {
+    "revision": "3e545c56b2c4cb98c9c4",
+    "url": "/ecommerce/static/js/16.436a4db4.chunk.js"
+  },
+  {
+    "revision": "5fd5d89c4338ebbe1edd",
+    "url": "/ecommerce/static/js/17.dcc0b9c5.chunk.js"
+  },
+  {
+    "revision": "f7daaaab0bdbda7b8bf8",
+    "url": "/ecommerce/static/js/18.705c35dc.chunk.js"
+  },
+  {
+    "revision": "831e7df421dabf99e1e1",
+    "url": "/ecommerce/static/js/19.40394672.chunk.js"
+  },
+  {
+    "revision": "737a855a5c69923e0330",
+    "url": "/ecommerce/static/js/2.33ed4f0f.chunk.js"
+  },
+  {
+    "revision": "53d4b266e1f0243c08ef",
+    "url": "/ecommerce/static/js/20.54924596.chunk.js"
+  },
+  {
+    "revision": "798a14c0bce1cc5edcf1",
+    "url": "/ecommerce/static/js/21.083f4473.chunk.js"
+  },
+  {
+    "revision": "fd810e57a6d3a24debe9",
+    "url": "/ecommerce/static/js/22.318be7e7.chunk.js"
+  },
+  {
+    "revision": "e8994746966c864cd6b4",
+    "url": "/ecommerce/static/js/23.877dbd60.chunk.js"
+  },
+  {
+    "revision": "aa2d77639731b7596cd2",
+    "url": "/ecommerce/static/js/24.e53992a9.chunk.js"
+  },
+  {
+    "revision": "e97c73963dffc4fd2105",
+    "url": "/ecommerce/static/js/25.9a645e08.chunk.js"
+  },
+  {
+    "revision": "7ad11ee13dd7db393789",
+    "url": "/ecommerce/static/js/26.093c90c4.chunk.js"
+  },
+  {
+    "revision": "401457a86abccb65f934",
+    "url": "/ecommerce/static/js/27.d921f23b.chunk.js"
+  },
+  {
+    "revision": "68a8417a1e8b421f3aa3",
+    "url": "/ecommerce/static/js/28.b1bbc525.chunk.js"
+  },
+  {
+    "revision": "3256ef19c83d5684cf89",
+    "url": "/ecommerce/static/js/29.e2409192.chunk.js"
+  },
+  {
+    "revision": "2a792def892c3949e0a5",
+    "url": "/ecommerce/static/js/3.0405d748.chunk.js"
+  },
+  {
+    "revision": "d6feb57fa41a1fe975c9",
+    "url": "/ecommerce/static/js/30.88d8b72e.chunk.js"
+  },
+  {
+    "revision": "05c7a0fda71bd37d780e",
+    "url": "/ecommerce/static/js/31.e41c1158.chunk.js"
+  },
+  {
+    "revision": "2b0f79dfd966fab74ec3",
+    "url": "/ecommerce/static/js/32.9bef815e.chunk.js"
+  },
+  {
+    "revision": "2850d6c1903bf605ca62",
+    "url": "/ecommerce/static/js/33.1cbb1378.chunk.js"
+  },
+  {
+    "revision": "964ff9186d69b3f6dc80",
+    "url": "/ecommerce/static/js/34.ea3fe192.chunk.js"
+  },
+  {
+    "revision": "ccff1a431a94a6728782",
+    "url": "/ecommerce/static/js/35.52328c50.chunk.js"
+  },
+  {
+    "revision": "008a575bd836dde688e1",
+    "url": "/ecommerce/static/js/36.0b5a5183.chunk.js"
+  },
+  {
+    "revision": "30cb469dd448ac878468",
+    "url": "/ecommerce/static/js/37.6e8cb953.chunk.js"
+  },
+  {
+    "revision": "d2f51aa3065115463a24",
+    "url": "/ecommerce/static/js/38.39ccfbf3.chunk.js"
+  },
+  {
+    "revision": "5d129cf4f4cd3bd3be0b",
+    "url": "/ecommerce/static/js/39.12eed1f0.chunk.js"
+  },
+  {
+    "revision": "f70528dbcba068a50f40",
+    "url": "/ecommerce/static/js/4.b829be33.chunk.js"
+  },
+  {
+    "revision": "659e0e8ee2e3b2b948bf",
+    "url": "/ecommerce/static/js/40.ddcd25dc.chunk.js"
+  },
+  {
+    "revision": "830bfc6dd19b05473dce",
+    "url": "/ecommerce/static/js/41.f7d61752.chunk.js"
+  },
+  {
+    "revision": "4ee038eb9a377ebd28df",
+    "url": "/ecommerce/static/js/42.9e79d03e.chunk.js"
+  },
+  {
+    "revision": "5b12a278fba1a01f4d2b",
+    "url": "/ecommerce/static/js/43.71597782.chunk.js"
+  },
+  {
+    "revision": "e190334b1be8c46d8610",
+    "url": "/ecommerce/static/js/44.99865fbd.chunk.js"
+  },
+  {
+    "revision": "205d475031fefbe1dfbb",
+    "url": "/ecommerce/static/js/45.8e59a34e.chunk.js"
+  },
+  {
+    "revision": "c26cdb50e373e16a6755",
+    "url": "/ecommerce/static/js/46.4433a342.chunk.js"
+  },
+  {
+    "revision": "a939a9821924dcf2b294",
+    "url": "/ecommerce/static/js/47.7d0a0de0.chunk.js"
+  },
+  {
+    "revision": "f4dbed5d410be59a43ba",
+    "url": "/ecommerce/static/js/48.87ef80db.chunk.js"
+  },
+  {
+    "revision": "560d951c10c90fdeb436",
+    "url": "/ecommerce/static/js/49.9e6278ff.chunk.js"
+  },
+  {
+    "revision": "ca546fb9c736ca9835b0",
+    "url": "/ecommerce/static/js/5.1a37653d.chunk.js"
+  },
+  {
+    "revision": "eee6d35725a31552174a",
+    "url": "/ecommerce/static/js/50.66fcab17.chunk.js"
+  },
+  {
+    "revision": "5644de5accd73aceebd4",
+    "url": "/ecommerce/static/js/51.6b3cf831.chunk.js"
+  },
+  {
+    "revision": "528b37b55feb4d11a122",
+    "url": "/ecommerce/static/js/52.742e5978.chunk.js"
+  },
+  {
+    "revision": "6f2944d1bd5e0cc36e18",
+    "url": "/ecommerce/static/js/53.c8c008c7.chunk.js"
+  },
+  {
+    "revision": "ad551dfa5643e4683a55",
+    "url": "/ecommerce/static/js/54.3ef6eb71.chunk.js"
+  },
+  {
+    "revision": "90daf88b4f6d0cac2116",
+    "url": "/ecommerce/static/js/55.b61b9c7a.chunk.js"
+  },
+  {
+    "revision": "3e56b1ac94dcdbe0e55b",
+    "url": "/ecommerce/static/js/56.8b5e44dd.chunk.js"
+  },
+  {
+    "revision": "3438defcae8410160a05",
+    "url": "/ecommerce/static/js/57.666908d1.chunk.js"
+  },
+  {
+    "revision": "16d2fd3ff5c97e9ccca0",
+    "url": "/ecommerce/static/js/58.2fc507b4.chunk.js"
+  },
+  {
+    "revision": "18763e4d578ca9437b83",
+    "url": "/ecommerce/static/js/59.bf3505fe.chunk.js"
+  },
+  {
+    "revision": "1ff54f052d751dc37e18",
+    "url": "/ecommerce/static/js/6.064e757c.chunk.js"
+  },
+  {
+    "revision": "6ab20da2d5b5ea4a5b56",
+    "url": "/ecommerce/static/js/60.7bb7c9cc.chunk.js"
+  },
+  {
+    "revision": "9fd979da7702951793ec",
+    "url": "/ecommerce/static/js/61.d59fe83d.chunk.js"
+  },
+  {
+    "revision": "5c2ecb2cb1c8a43ac8b2",
+    "url": "/ecommerce/static/js/62.8dc3a250.chunk.js"
+  },
+  {
+    "revision": "dea3c7964edcf2fa938f",
+    "url": "/ecommerce/static/js/63.41b9ba01.chunk.js"
+  },
+  {
+    "revision": "8df6076c10e6294b048f",
+    "url": "/ecommerce/static/js/64.42dcd330.chunk.js"
+  },
+  {
+    "revision": "d87df7fa11237fc50f4b",
+    "url": "/ecommerce/static/js/65.b25a64dd.chunk.js"
+  },
+  {
+    "revision": "928ad6a1cf22f95d448f",
+    "url": "/ecommerce/static/js/66.4afa18e9.chunk.js"
+  },
+  {
+    "revision": "bf932004a387e7dcdabf",
+    "url": "/ecommerce/static/js/67.1819f280.chunk.js"
+  },
+  {
+    "revision": "fb062a4890741a8bb108",
+    "url": "/ecommerce/static/js/9.9fb655b4.chunk.js"
+  },
+  {
+    "revision": "b47e5b8519c1b9510524",
+    "url": "/ecommerce/static/js/main.2bd9c908.chunk.js"
+  },
+  {
+    "revision": "1495ac04a80d708e6312",
+    "url": "/ecommerce/static/js/runtime-main.c083f265.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/ecommerce/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/ecommerce/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/ecommerce/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/ecommerce/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/ecommerce/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/ecommerce/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/ecommerce/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/ecommerce/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/ecommerce/static/media/fontawesome-webfont.f775f9cc.svg"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/ecommerce/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/ecommerce/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/ecommerce/static/media/lg.f4292655.ttf"
+  }
+]);
